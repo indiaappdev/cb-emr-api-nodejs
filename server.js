c8c8a001fce48be74bf881f19e54a516b7120d1e
KaushikOgmaTango <kauhsik.roy@appstango.com>
2024-05-15 00:23:37 +0530

    server.js updated fro bug fix

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post('/sendInvoice', async (req, res) => {
         }
     } catch (error) {
             // Log the error, assuming logger is set up for error reporting
-            logger.error("send email:: error - " + error.stack);
+            console.log("send email:: error - " + error.stack);
           }
 });
 
